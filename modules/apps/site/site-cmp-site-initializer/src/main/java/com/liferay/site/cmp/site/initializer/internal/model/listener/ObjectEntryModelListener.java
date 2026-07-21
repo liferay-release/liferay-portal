d3713c3e957dc2c1493cb5c246e407879aacf914
Yuri Monteiro <yuri.monteiro@liferay.com>
2026-07-21 15:31:48 -0300

    LPD-97887 Sync project manager and project sponsor roles on project creation

--- modules/apps/site/site-cmp-site-initializer/src/main/java/com/liferay/site/cmp/site/initializer/internal/model/listener/ObjectEntryModelListener.java
+++ modules/apps/site/site-cmp-site-initializer/src/main/java/com/liferay/site/cmp/site/initializer/internal/model/listener/ObjectEntryModelListener.java
@@ -97,6 +97,8 @@ public class ObjectEntryModelListener extends BaseModelListener<ObjectEntry> {
 			_route("CMP_ADD_ASSET", objectEntry);
 			_setResourcePermissions(objectEntry);
 			_updateGroup(objectEntry);
+			_updateProjectManagerProjectSponsorUserGroupRoles(
+				null, objectEntry);
 
 			CMPProjectObjectEntryValuesUtil.updateCompletionRate(
 				objectEntry, _filterFactory);
@@ -524,9 +526,10 @@ public class ObjectEntryModelListener extends BaseModelListener<ObjectEntry> {
 		long projectManagerUserId = MapUtil.getLong(
 			objectEntry.getValues(), "r_userToCMPProjectManager_userId", 0);
 
-		if (projectManagerUserId != MapUtil.getLong(
+		if ((originalObjectEntry == null) ||
+			(projectManagerUserId != MapUtil.getLong(
 				originalObjectEntry.getValues(),
-				"r_userToCMPProjectManager_userId", 0)) {
+				"r_userToCMPProjectManager_userId", 0))) {
 
 			_updateUserGroupRoles(
 				objectEntry.getGroupId(),
@@ -537,9 +540,10 @@ public class ObjectEntryModelListener extends BaseModelListener<ObjectEntry> {
 		long projectSponsorUserId = MapUtil.getLong(
 			objectEntry.getValues(), "r_userToCMPProjectSponsor_userId", 0);
 
-		if (projectSponsorUserId != MapUtil.getLong(
+		if ((originalObjectEntry == null) ||
+			(projectSponsorUserId != MapUtil.getLong(
 				originalObjectEntry.getValues(),
-				"r_userToCMPProjectSponsor_userId", 0)) {
+				"r_userToCMPProjectSponsor_userId", 0))) {
 
 			_updateUserGroupRoles(
 				objectEntry.getGroupId(),
