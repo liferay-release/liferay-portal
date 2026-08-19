913e634f56e4a2345ac4f74e53bb9d11428f4c76
Yuri Monteiro <yuri.monteiro@liferay.com>
2026-08-19 15:13:46 -0300

    LPD-102016 Revoke the project role of a replaced project manager or project sponsor

--- modules/apps/site/site-cmp-site-initializer/src/main/java/com/liferay/site/cmp/site/initializer/internal/model/listener/ObjectEntryModelListener.java
+++ modules/apps/site/site-cmp-site-initializer/src/main/java/com/liferay/site/cmp/site/initializer/internal/model/listener/ObjectEntryModelListener.java
@@ -42,7 +42,9 @@ import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.ResourceActionLocalService;
@@ -51,6 +53,7 @@ import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserGroupRoleService;
 import com.liferay.portal.kernel.service.UserService;
+import com.liferay.portal.kernel.service.permission.UserPermissionUtil;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.CalendarFactoryUtil;
 import com.liferay.portal.kernel.util.ListUtil;
@@ -152,6 +155,27 @@ public class ObjectEntryModelListener extends BaseModelListener<ObjectEntry> {
 		}
 	}
 
+	private void _deleteGroupMembershipWithoutUserGroupRoles(
+			long groupId, long userId)
+		throws Exception {
+
+		if (ListUtil.isNotEmpty(
+				_roleLocalService.getUserGroupRoles(userId, groupId))) {
+
+			return;
+		}
+
+		User user = _userService.getUserById(userId);
+
+		_updateUser(ArrayUtil.remove(user.getGroupIds(), groupId), userId);
+
+		if (_groupLocalService.hasUserGroup(userId, groupId)) {
+			throw new PrincipalException.MustHavePermission(
+				PermissionThreadLocal.getPermissionChecker(),
+				Group.class.getName(), groupId, ActionKeys.ASSIGN_MEMBERS);
+		}
+	}
+
 	private void _deleteObjectEntries(ObjectEntry objectEntry)
 		throws Exception {
 
@@ -196,6 +220,35 @@ public class ObjectEntryModelListener extends BaseModelListener<ObjectEntry> {
 		}
 
 		_depotEntryLocalService.deleteDepotEntry(depotEntry);
+	}
+
+	private void _deleteUserGroupRoles(
+			long companyId, long groupId, List<String> roleNames, long userId)
+		throws Exception {
+
+		UserPermissionUtil.check(
+			PermissionThreadLocal.getPermissionChecker(), userId,
+			ActionKeys.UPDATE);
+
+		long[] roleIds = TransformUtil.transformToLongArray(
+			roleNames,
+			roleName -> {
+				Role role = _roleLocalService.fetchRole(companyId, roleName);
+
+				if (role == null) {
+					return null;
+				}
+
+				return role.getRoleId();
+			});
+
+		if (roleIds.length == 0) {
+			return;
+		}
+
+		_userGroupRoleService.deleteUserGroupRoles(userId, groupId, roleIds);
+
+		_deleteGroupMembershipWithoutUserGroupRoles(groupId, userId);
 	}
 
 	private ObjectEntry _fetchLinkedObjectEntry(
@@ -281,6 +334,16 @@ public class ObjectEntryModelListener extends BaseModelListener<ObjectEntry> {
 		}
 
 		return new String[] {ActionKeys.ADD_DISCUSSION, ActionKeys.VIEW};
+	}
+
+	private long _getRelatedUserId(
+		ObjectEntry objectEntry, String objectFieldName) {
+
+		if (objectEntry == null) {
+			return 0;
+		}
+
+		return MapUtil.getLong(objectEntry.getValues(), objectFieldName, 0);
 	}
 
 	private void _initializeSite(ObjectEntry objectEntry) {
@@ -523,33 +586,18 @@ public class ObjectEntryModelListener extends BaseModelListener<ObjectEntry> {
 			return;
 		}
 
-		long projectManagerUserId = MapUtil.getLong(
-			objectEntry.getValues(), "r_userToCMPProjectManager_userId", 0);
-
-		if ((originalObjectEntry == null) ||
-			(projectManagerUserId != MapUtil.getLong(
-				originalObjectEntry.getValues(),
-				"r_userToCMPProjectManager_userId", 0))) {
-
-			_updateUserGroupRoles(
-				objectEntry.getGroupId(),
-				Collections.singletonList(DepotRolesConstants.PROJECT_MANAGER),
-				projectManagerUserId);
-		}
-
-		long projectSponsorUserId = MapUtil.getLong(
-			objectEntry.getValues(), "r_userToCMPProjectSponsor_userId", 0);
-
-		if ((originalObjectEntry == null) ||
-			(projectSponsorUserId != MapUtil.getLong(
-				originalObjectEntry.getValues(),
-				"r_userToCMPProjectSponsor_userId", 0))) {
-
-			_updateUserGroupRoles(
-				objectEntry.getGroupId(),
-				Collections.singletonList(DepotRolesConstants.PROJECT_MEMBER),
-				projectSponsorUserId);
-		}
+		_updateUserGroupRoles(
+			objectEntry.getCompanyId(), objectEntry.getGroupId(),
+			_getRelatedUserId(
+				originalObjectEntry, "r_userToCMPProjectManager_userId"),
+			Collections.singletonList(DepotRolesConstants.PROJECT_MANAGER),
+			_getRelatedUserId(objectEntry, "r_userToCMPProjectManager_userId"));
+		_updateUserGroupRoles(
+			objectEntry.getCompanyId(), objectEntry.getGroupId(),
+			_getRelatedUserId(
+				originalObjectEntry, "r_userToCMPProjectSponsor_userId"),
+			Collections.singletonList(DepotRolesConstants.PROJECT_MEMBER),
+			_getRelatedUserId(objectEntry, "r_userToCMPProjectSponsor_userId"));
 	}
 
 	private User _updateUser(long[] groupIds, Long userId) throws Exception {
@@ -577,8 +625,18 @@ public class ObjectEntryModelListener extends BaseModelListener<ObjectEntry> {
 	}
 
 	private void _updateUserGroupRoles(
-			long groupId, List<String> roleNames, long userId)
+			long companyId, long groupId, long originalUserId,
+			List<String> roleNames, long userId)
 		throws Exception {
+
+		if (originalUserId == userId) {
+			return;
+		}
+
+		if (originalUserId != 0) {
+			_deleteUserGroupRoles(
+				companyId, groupId, roleNames, originalUserId);
+		}
 
 		if (userId == 0) {
 			return;
@@ -588,8 +646,6 @@ public class ObjectEntryModelListener extends BaseModelListener<ObjectEntry> {
 
 		user = _updateUser(
 			ArrayUtil.append(user.getGroupIds(), groupId), userId);
-
-		long companyId = user.getCompanyId();
 
 		_userGroupRoleService.addUserGroupRoles(
 			user.getUserId(), groupId,
